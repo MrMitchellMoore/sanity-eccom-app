deecba3535e6ef9034b17a20346d7e1bf2a6125a
Mitchell Moore <mmoorejr24@gmail.com>
2023-11-26 08:17:52 -0800

    First Commit

--- sanity-backend/sanity/sanity.cli.ts
+++ sanity-backend/sanity/sanity.cli.ts
@@ -2,7 +2,7 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'l4gxqn2g',
-    dataset: 'production',
+    projectId: process.env.PROJECTID as string,
+    dataset: process.env.DATASET as string,
   },
 })
--- sanity-backend/sanity/sanity.config.ts
+++ sanity-backend/sanity/sanity.config.ts
@@ -3,11 +3,18 @@ import {deskTool} from 'sanity/desk'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
+const config = {
+  projectId: (process.env.PROJECTID as string) || 'l4gxqn2g',
+  dataset: (process.env.DATASET as string) || 'production',
+  name: process.env.NEXT_PUBLIC_NAME as string,
+  title: process.env.NEXT_PUBLIC_TITLE as string,
+}
+
 export default defineConfig({
-  name: 'default',
-  title: 'ecommerce-sanity-app',
-  projectId: 'l4gxqn2g',
-  dataset: 'production',
+  name: config.name,
+  title: config.title,
+  projectId: config.projectId,
+  dataset: config.dataset,
   plugins: [deskTool(), visionTool()],
   schema: {
     types: schemaTypes,
